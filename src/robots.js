b85c1abcfc2701b27310cd9fc347d7dcefe65720
cuttell2000 <cuttell2000@yahoo.com>
2021-01-31 20:33:07 -0600

    updating project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Osuofia Onye',
+    username: 'Maya',
+    email: 'awuluma@paddy.biz'
+  },
+  {
+    id: 2,
+    name: 'Gozza Toroto',
+    username: 'Odumu',
+    email: 'awuluoku@suarezz.tv'
+  },
+  {
+    id: 3,
+    name: 'Ndukwuwa Oyelele',
+    username: 'Ehele',
+    email: 'lamuike@connectus.net'
+  },
+  {
+    id: 4,
+    name: 'Gwaskwa Ndiyard',
+    username: 'Kosikwado',
+    email: 'Jayejaye.ifedil@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
